a634ee4cb9a459288e60426eda6ec03d0655099a
otaviorodriguess <otaviorodriguesbezerra71@gmail.com>
2026-02-23 20:38:43 -0300

    Configurações iniciais do backend e dependências Maven

---
+++ src/main/java/com/otavio/beautybooking/BeautybookingApplication.java
@@ -0,0 +1,13 @@
+package com.otavio.beautybooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautybookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautybookingApplication.class, args);
+	}
+
+}
